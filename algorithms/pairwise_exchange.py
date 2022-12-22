0fb5f6a45964c9a56d58a1b1c42e302602407fc2
Tom <tom.palleau@gmail.com>
2022-12-22 13:26:11 +0100

    weight updated on reverse path graphs
    also optimised the path on ring graph are only updated on smallest ring graph found

--- algorithms/pairwise_exchange.py
+++ algorithms/pairwise_exchange.py
@@ -21,29 +21,30 @@ def multiNodes_to_starGraph(graph: nx.MultiDiGraph, multi_nodes: list[nx.nodes],
     
     return star_graph
 
-def starGraph_to_ringGraph(starg_graph: nx.DiGraph) -> nx.DiGraph:
+def starGraph_to_ringGraph(star_graph: nx.DiGraph) -> nx.DiGraph:
     ring_graph = nx.DiGraph()
     
     # randomly pick a node
-    start_node = list(starg_graph.nodes)[2]
+    start_node = list(star_graph.nodes)[0]
     
     # get nearest node
-    nearest_node, properties = min(starg_graph[start_node].items(), key=lambda edge: edge[1]["weight"])
+    nearest_node, properties = min(star_graph[start_node].items(), key=lambda edge: edge[1]["weight"])
     
-    ring_graph.add_edge(start_node, nearest_node, **properties)
+    # add weight and node
+    ring_graph.add_edge(start_node, nearest_node, weight = properties["weight"])
     
     search_node = nearest_node
     
-    while not all(node in ring_graph for node in starg_graph.nodes):
+    while not all(node in ring_graph for node in star_graph.nodes):
         # get a sorted list of the nearest node
-        sorted_nearest_nodes = sorted(starg_graph[search_node].items(), key=lambda edge: edge[1]["weight"])
+        sorted_nearest_nodes = sorted(star_graph[search_node].items(), key=lambda edge: edge[1]["weight"])
         for nearest_node, properties in sorted_nearest_nodes:
             if nearest_node not in ring_graph:
-                ring_graph.add_edge(search_node, nearest_node, **properties)
+                ring_graph.add_edge(search_node, nearest_node, weight = properties["weight"])
                 search_node = nearest_node
                 break
     
-    ring_graph.add_edge(search_node, start_node, weight=starg_graph[start_node][nearest_node]["weight"])
+    ring_graph.add_edge(search_node, start_node, weight=star_graph[start_node][nearest_node]["weight"])
 
     return ring_graph
 
@@ -57,14 +58,14 @@ def exchange_nodes(star_graph: nx.DiGraph, ring_graph: nx.DiGraph, recursion: in
     
     #get a random edge and convert to list
     switch_edge1 = list(edges.pop(edges.index(choice(edges))))
-    switch_edge1.append(star_graph.get_edge_data(*switch_edge1))
+    switch_edge1.append(star_graph.get_edge_data(*switch_edge1, "weight"))
     
     # remove neighbor edges of the nodes in switch_edge1
     edges = [edge for edge in edges if not(switch_edge1[0] in edge or switch_edge1[1] in edge)]
 
     #get a random edge and convert to list
     switch_edge2 = list(edges.pop(edges.index(choice(edges))))
-    switch_edge2.append(star_graph.get_edge_data(*switch_edge1))
+    switch_edge2.append(star_graph.get_edge_data(*switch_edge2, "weight"))
     ring_graph.remove_edges_from([switch_edge1, switch_edge2])
     
     # swap edges
@@ -80,24 +81,34 @@ def exchange_nodes(star_graph: nx.DiGraph, ring_graph: nx.DiGraph, recursion: in
     path_graph_B.remove_nodes_from(list_path_graph_nodes[1])
     
     # find the path graph to reverse
+    graph_reversed = None
     if switch_edge1[1] in path_graph_A:
         if path_graph_A.predecessors(switch_edge1[1]):
             path_graph_A = path_graph_A.reverse()
+            graph_reversed = path_graph_A
         else:
             path_graph_B = path_graph_B.reverse()
+            graph_reversed = path_graph_B
     else:
         if path_graph_B.predecessors(switch_edge1[1]):
             path_graph_B = path_graph_B.reverse()
+            graph_reversed = path_graph_B
         else:
             path_graph_A = path_graph_A.reverse()
+            graph_reversed = path_graph_A
     
+    # update weights of reversed path
+    weighted_edges = [(*edge, star_graph[edge[0]][edge[1]]["weight"]) for edge in graph_reversed.edges]
+    graph_reversed.add_weighted_edges_from(weighted_edges)
+    
+    # reconstruct graph
     ring_graph = nx.union(path_graph_A, path_graph_B)
     ring_graph.add_edges_from([switch_edge1, switch_edge2])
     
     print(f"recursion: {recursion} size: ", ring_graph.size(weight="weight"))
     
     # return swapped graph
-    if recursion == 0:
+    if recursion == 1:
         return ring_graph
     
     new_ring_graph = exchange_nodes(star_graph, ring_graph.copy(), recursion - 1)
@@ -114,5 +125,9 @@ def pairwise_exchange(graph, multinodes: list[nx.nodes], recursion) -> nx.graph:
     ring_graph = starGraph_to_ringGraph(star_graph)
     
     star_graph = exchange_nodes(star_graph, ring_graph, recursion)
+    
+    # add paths to ring graph
+    for edge in ring_graph.edges:
+        ring_graph[edge[0]][edge[1]]["path"] = star_graph.get_edge_data(*edge, "path")
     
     return star_graph
